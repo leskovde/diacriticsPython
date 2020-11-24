49f10e4f7cb1e85c46b656c23b8085bbd1066cb1
Vojtěch Švandelík <vojtech.svandelik@gmail.com>
2020-11-24 11:38:53 +0100

    added support for downloading dictionary

--- main.py
+++ main.py
@@ -55,6 +55,22 @@ class Dataset:
             self.target = dataset_file.read()
         self.data = self.target.translate(self.DIA_TO_NODIA)
 
+class Dictionary:
+    def __init__(self,
+                 name="fiction-dictionary.txt",
+                 url="https://ufal.mff.cuni.cz/~straka/courses/npfl129/2021/datasets/"):
+        if not os.path.exists(name):
+            print("Downloading {}...".format(name), file=sys.stderr)
+            urllib.request.urlretrieve(url + name, filename=name)
+            urllib.request.urlretrieve(url + name.replace(".txt", ".LICENSE"), filename=name.replace(".txt", ".LICENSE"))
+
+        # Load the dictionary to `variants`
+        self.variants = {}
+        with open(name, "r", encoding="utf-8") as dictionary_file:
+            for line in dictionary_file:
+                nodia_word, *variants = line.rstrip("\n").split()
+                self.variants[nodia_word] = variants
+
 
 parser = argparse.ArgumentParser()
 # These arguments will be set appropriately by ReCodEx, even if you change them.
